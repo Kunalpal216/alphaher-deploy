7970b1f873ac90f1c8005b52776ea059109e3eb4
Kunalpal216 <kunalpal2115@gmail.com>
2024-06-19 20:10:28 +0530

    updated dockerfile

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: process.env.BASE_PATH || process.env.CUSTOMCONNSTR_BASE_PATH,
-    reactStrictMode: false,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     basePath: process.env.BASE_PATH || process.env.CUSTOMCONNSTR_BASE_PATH,
+//     reactStrictMode: false,
+// }
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
